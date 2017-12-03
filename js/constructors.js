203dcbf36b48772f3fa86fce3d6f54f9ec476a80
F4Tornado <h.rovnyak@gmail.com>
2017-12-03 08:36:30 -0500

    stuff happens when a button is clicked

--- js/constructors.js
+++ js/constructors.js
@@ -1,16 +1,18 @@
-function button(x, y, width, height, colory) {
+function button(x, y, width, height, colory, onClick) {
   this.x = x;
   this.y = y;
   this.width = width;
   this.height = height;
   this.color = colory;
+  this.onClick = onClick
   this.show = () => {
     fill(this.color);
     rect(this.x, this.y, this.width, this.height);
   }
   this.testClick = () => {
     if (this.x <= mouseX && this.x + this.width >= mouseX && this.y <= mouseY && this.y+this.height >= mouseY) {
-      console.log(true);
+      onClick();
+      return true;
     }
   }
 }
